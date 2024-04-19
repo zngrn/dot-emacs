4a146107801100f07ed9180347169c870651f513
zngrn <vaibhavjhingran@gmail.com>
2024-04-19 11:09:33 +0200

    Apply + inspect clojure setup

--- forge/init-lsp.el
+++ forge/init-lsp.el
@@ -4,10 +4,16 @@
 
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
+  :hook ((clojure-mode . lsp)
+	 (clojurec-mode . lsp)
+	 (clojurescript-mode . lsp)
+	 (web-mode . lsp))
   :init
   (setq lsp-keymap-prefix "C-c l")
   :config
-  (lsp-enable-which-key-integration t))
+  (lsp-enable-which-key-integration t)
+  ;; add paths to local installation of tools like lein
+  (setenv "PATH" (concat "/usr/local/bin" path-separator (getenv "PATH"))))
 
 ;; tree-view of code hierarchy
 (use-package lsp-treemacs
@@ -15,5 +21,10 @@
 
 ;; jump to anything in a project by name lookup
 (use-package lsp-ivy)
+
+;; lsp-ui -> use when needed, enable it if required
+(use-package lsp-ui
+  :disabled t
+  :commands lsp-ui-mode)
 
 (provide 'init-lsp)
