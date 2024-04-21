c54393fa2aa8f5df21cfc0ff45a22718670e320d
zngrn <vaibhavjhingran@gmail.com>
2024-04-21 03:35:50 +0200

    Use a working version of lsp golang formatting + update readme for golang setup

--- README.md
+++ README.md
@@ -7,7 +7,13 @@
 
   * git clone: `https://github.com/zngrn/dot-emacs.git ~/.emacs.d`
 
-  * To use clojure with lsp:
+  * To use `clojure` with lsp:
     - Install manually: [clojure-lsp](https://clojure-lsp.io/installation/#manually)
   * Check clojure lsp:
     - `which clojure-lsp` should prompt a sane location
+
+  * To use `golang` with lsp:
+    - Install `gopls` manually: `go install golang.org/x/tools/gopls@latest
+`.
+  * Check `gopls`:
+    - `~/go/bin/gopls version` should prompt a version
--- forge/init-go.el
+++ forge/init-go.el
@@ -5,17 +5,19 @@
 (use-package go-mode
   :mode "\\.go\\'"
   :ensure t
-  :hook (go-mode . (lambda ()
-                     (add-hook 'before-save-hook 'gofmt-before-save nil t)))
   :config
   (setq gofmt-command "goimports")
-  (add-to-list 'exec-path (expand-file-name "~/go/bin"))
-  )
+  (add-to-list 'exec-path (expand-file-name "~/go/bin")))
 
 ;; path to gopls
 (setq lsp-gopls-server-path "~/go/bin/gopls")
 
 ;; static analysis
 (setq lsp-gopls-staticcheck t)
+
+;; leverage lsp for go
+(setq lsp-gopls-staticcheck t)
+(setq lsp-eldoc-render-all t)
+(setq lsp-gopls-complete-unimported t)
 
 (provide 'init-go)
